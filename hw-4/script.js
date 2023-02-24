7b4d45d030a7cd3b49255185ceebd79eab4a1038
Koreandraw <dacolt@yandex.ru>
2023-02-24 20:31:26 +0400

    add HW4

---
+++ hw-4/script.js
@@ -0,0 +1,70 @@
+//1
+/*
+let i = 0;
+
+do {
+  console.log('Привет');
+  i++;
+} while (i < 2);
+
+//2
+let i = 1;
+
+do {
+    console.log(i);
+    i++;
+} while (i <= 5);
+
+//3
+let i = 7;
+
+do {
+    console.log(i);
+    i++;
+} while (i <= 22);
+
+//4
+const obj = {
+	'Коля': 200,
+    'Вася': 300,
+    'Петя': 400	
+}
+
+for (let name in obj) {
+    console.log(`${name} - зарплата ${obj[name]} долларов`);  
+}
+
+//5
+for (let number = 1000, num = 0; number > 50; num++) {
+    number /= 2;
+    console.log(num);    
+}
+
+//6
+for (let friday = 3; friday < 31; friday++) {
+    if (friday===3) {
+        console.log(`Сегодня пятница, ${friday}-е число. Необходимо подготовить отчет.`);               
+    }    
+    else if (friday===10) {
+        console.log(`Сегодня пятница, ${friday}-е число. Необходимо подготовить отчет.`);
+    }
+    else if (friday===17) {
+        console.log(`Сегодня пятница, ${friday}-е число. Необходимо подготовить отчет.`);
+    }
+    else if (friday===24) {
+        console.log(`Сегодня пятница, ${friday}-е число. Необходимо подготовить отчет.`);
+    }
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
