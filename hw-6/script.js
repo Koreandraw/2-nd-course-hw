8e03207287d617f8320d9211731e11622751143a
Koreandraw <dacolt@yandex.ru>
2023-03-05 19:44:10 +0400

    fix

--- hw-6/script.js
+++ hw-6/script.js
@@ -83,14 +83,15 @@ let result = arr.flat(1);
 
 console.log(result);
 
-//10
+//10 исправил
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let result = 0;
-for (let i = 0; i < arr.length; i++) {
-    result += arr[i];    
+
+for (let i = 0; i < arr.length - 1; i++) {
+    console.log(arr[i] + arr[i+1]);
 }
-console.log(result);
 */
+
 
 
 
